849ad9ec1505349124d9c7b36faa62c14977065f
sarahgibsonnnn <sarah_gibson2@hotmail.co.uk>
2022-09-01 20:03:33 +0100

    Improved styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -20,7 +20,7 @@ export default function Dictionary() {
     }
     return <div className = "Dictionary">
         <form onSubmit={search} >
-            <input type="search" placeholder="Search.." autoFocus={true} onChange={handleKeywordChange}/>
+            <input className="form-control" type="search" placeholder="Search.." autoFocus={true} onChange={handleKeywordChange}/>
         </form>
         <Results results={results}/>
     </div>
--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props) {
     if(props.results) {
         return (
             <div className = "Results">
-               <h2>{props.results.word}</h2>
+               <h1>{props.results.word}</h1>
                {props.results.meanings.map(function(meaning, index) {
                 
                  return <div key={index}>
