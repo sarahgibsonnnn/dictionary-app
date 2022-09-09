820205df42506dacdf67f851e09070c32669481e
sarahgibsonnnn <sarah_gibson2@hotmail.co.uk>
2022-09-09 17:34:30 +0100

    Added rel="noreferrer" to phonetics link

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -9,7 +9,7 @@ export default function Phonetics(props) {
                 return (
                 <div className="phonetic">
                     <span key={index}>{phonetic.text}</span>
-                     <a href={phonetic.audio} target="_blank" style={{display: phonetic.audio ? 'block' : 'none'}}> Listen </a>
+                     <a href={phonetic.audio} target="_blank" rel="noreferrer" style={{display: phonetic.audio ? 'block' : 'none'}}> Listen </a>
                 </div>
                 )
             })}
